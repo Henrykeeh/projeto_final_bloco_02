43980ed5225dcb9f97646febfa8b678366b511a2
Henrykeeh <c.henrique.silvabarbosa@gmail.com>
2025-05-05 11:02:56 -0300

    Criada a tabela produto e seu relacionamento com a tabela categoria, criada o CRUD completo da tabela produto

--- src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.projeto_final_bloco_02.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,7 +29,11 @@ public class Categoria {
 	@NotBlank
 	@Size(min = 3, max = 500)
 	private String categoria;
-			
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+				
 	//Getters and Setters
 
 	public Long getId() {
@@ -44,4 +55,15 @@ public class Categoria {
 		this.categoria = categoria;
 	}
 
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 }
---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Produto.java
@@ -0,0 +1,80 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(min = 5, max = 255)
+	private String nome;
+
+	@NotNull
+	private BigDecimal preco;
+
+	@NotNull
+	private Boolean restricao;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	// Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Boolean getRestricao() {
+		return restricao;
+	}
+
+	public void setRestricao(Boolean restricao) {
+		this.restricao = restricao;
+	}
+}
